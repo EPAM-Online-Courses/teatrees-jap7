72cbc4d1d40fc7c9be995aabb6191411d92d7ff4
emmaline261 <emmaline261@gmail.com>
2022-03-15 15:51:47 +0100

    Implemented UBlock

--- src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
+++ src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
@@ -8,7 +8,7 @@ public class BlockFeed {
 
     private final Random rnd = new Random();
     private final List<Supplier<Block>> blocks = List.of(
-        OBlock::new
+        UBlock::new, OBlock::new
     );
 
     public BlockFeed() {
---
+++ src/main/java/com/epam/prejap/teatrees/block/UBlock.java
@@ -0,0 +1,13 @@
+package com.epam.prejap.teatrees.block;
+
+final class UBlock extends Block {
+
+    private static final byte[][] IMAGE = {
+            {1, 0, 1},
+            {1, 1, 1},
+    };
+
+    public UBlock() {
+        super(IMAGE);
+    }
+}
