2be0b556486d2ea8bc58317b443f541f6eef1841
emmaline261 <emmaline261@gmail.com>
2022-03-16 09:14:47 +0100

    BlockFeed tested

---
+++ src/test/java/com/epam/prejap/teatrees/block/BlockFeedTest.java
@@ -0,0 +1,23 @@
+package com.epam.prejap.teatrees.block;
+
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+import static org.testng.Assert.*;
+
+@Test
+public class BlockFeedTest {
+
+
+    public void shouldContainUBlock() {
+        // given
+        BlockFeed blockFeed = new BlockFeed();
+
+        // when
+        var actual = blockFeed.nextBlock(0);
+
+        // then
+        assertTrue(actual instanceof UBlock);
+    }
+}
