50d9a7410524b7bab467b5401cff64d38a19e9a2
herman_kulik <herman_kulik@epam.com>
2022-03-16 17:28:04 +0100

    add test for Result class

--- src/main/java/com/epam/prejap/teatrees/records/Result.java
+++ src/main/java/com/epam/prejap/teatrees/records/Result.java
@@ -9,17 +9,20 @@ import java.util.Scanner;
 public class Result {
 
     public void concludeTheGame(int score) {
-        String nameOfThePlayer = getUserName();
+        String nameOfThePlayer = getUserName(new Scanner(System.in));
         boolean isNewBestRecord = new RecordHandler().handleNewRecord(new Record(nameOfThePlayer, score));
+        printFinalMessage(isNewBestRecord, score, nameOfThePlayer);
+
+    }
+
+    void printFinalMessage(boolean isNewBestRecord, int score, String nameOfThePlayer) {
         System.out.println("\nScore: " + score);
         if (isNewBestRecord) {
             System.out.println("Congrats! It's your new record " + nameOfThePlayer + "!");
         }
     }
 
-    private String getUserName() {
-        Scanner scanner = new Scanner(System.in);
-        
+    String getUserName(Scanner scanner) {
         System.out.println("Type your name (only 3 letters allowed): ");
         String nameOfThePlayer = scanner.nextLine();
         while (nameOfThePlayer.length() != 3) {
---
+++ src/main/test/com/epam/prejap/teatrees/records/ResultTest.java
@@ -0,0 +1,68 @@
+package com.epam.prejap.teatrees.records;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * @author Herman Kulik
+ */
+public class ResultTest {
+
+    @Test
+    public void conclusionShouldWriteCorrectMessage() {
+        PrintStream console = System.out;
+        ByteArrayOutputStream bArray = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(bArray);
+        System.setOut(printStream);
+
+        Result result = new Result();
+        result.printFinalMessage( true, 1000, "har");
+        System.setOut(console);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(bArray.toString().contains("har"));
+        softAssert.assertTrue(bArray.toString().contains("Congrats! It's your new record!"));
+        softAssert.assertTrue(bArray.toString().contains("1000"));
+    }
+
+    @Test(dataProvider = "provideName")
+    public void nameShouldBeSetInConsole(String input, String output) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+        Result result = new Result();
+        String actualName = result.getUserName(new Scanner(byteArrayInputStream));
+
+        Assert.assertEquals(actualName, output);
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void rotateInEndlessLoopIfNameIsLongerThan_3_Letters() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("LONG_NAME".getBytes(StandardCharsets.UTF_8));
+
+        Result result = new Result();
+        String actualName = result.getUserName(new Scanner(byteArrayInputStream));
+
+        Assert.assertEquals(actualName, "LONG_NAME");
+    }
+
+    @DataProvider
+    public Object[][] provideName() {
+        return new Object[][]{
+                {"nad", "nad"},
+                {"sss", "sss"},
+                {"111", "111"},
+                {"   ", "   "},
+        };
+    }
+
+
+}
