72f2f7ec1d6a776f5e0474f768d237219a2dcf57
herman_kulik <herman_kulik@epam.com>
2022-03-16 16:09:24 +0100

    add first test for Record class

---
+++ src/main/test/com/epam/prejap/teatrees/records/RecordTest.java
@@ -0,0 +1,42 @@
+package com.epam.prejap.teatrees.records;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author Herman Kulik
+ */
+public class RecordTest {
+
+    @Test
+    public void recordsShouldBeEqual() {
+        Record first = new Record("one", 12397);
+        Record second = new Record("one", 2);
+        assertEquals(second, first, "Objects are different");
+    }
+
+    @Test
+    public void recordsShouldNotBeEqual() {
+        Record first = new Record("two", 2);
+        Record second = new Record("one", 2);
+        assertNotEquals(first, second, "Objects are the same");
+    }
+
+    @Test
+    public void recordShouldBePrintedCorrectly() {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        Record record = new Record("one", 1);
+        System.out.println(record);
+        System.setOut(console);
+
+        assertEquals(outputStream.toString(), "<<<player: one, top score: 1>>>\n", "Output is not the same");
+    }
+
+}
