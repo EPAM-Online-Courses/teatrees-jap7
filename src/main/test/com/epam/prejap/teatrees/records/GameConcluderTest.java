30f426710e8bca78b7ec1dea290855059ae3c63a
Andrzej Sadlek <andrzej_sadlek@epam.com>
2022-03-18 15:44:39 +0100

    Add basic test to GameConcluderTest

--- src/main/test/com/epam/prejap/teatrees/records/GameConcluderTest.java
+++ src/main/test/com/epam/prejap/teatrees/records/GameConcluderTest.java
@@ -2,12 +2,35 @@ package com.epam.prejap.teatrees.records;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class GameConcluderTest {
 
     @Test
-    public void gameIsConcludedWithNewPersonalRecord() {
-
+    public void gameIsConcludedSuccessfully() {
+        try {
+            InputStream console = System.in;
+            //given
+            int score = 20;
+            String name = "her";
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(name.getBytes(StandardCharsets.UTF_8));
+            System.setIn(byteArrayInputStream);
+            MessagePrinter messagePrinter = new MessagePrinter();
+            Path tmp = Files.createTempFile("temp2", ".json");
+            PrintStream ps = new PrintStream(tmp.toString());
+            ps.println("{'records': [{'name': 'her', 'score': 3}]}");
+            JSONParser jsonParser = new JSONParser(tmp.toFile());
+            GameConcluder gameConcluder = new GameConcluder();
+            //when
+            gameConcluder.concludeTheGame(score, tmp.toFile(), messagePrinter);
+            System.setIn(console);
+            //then
+        } catch (IOException e) {
+            System.err.println("Game can't conclude");
+        }
     }
+
 }
