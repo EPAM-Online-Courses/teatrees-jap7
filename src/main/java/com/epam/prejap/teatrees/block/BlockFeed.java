4bec25d251321e8940ac8cabc9502fbea9d66117
Pawel Kierat <pawel_kierat@epam.com>
2022-03-24 11:16:27 +0100

    Integration test for IBlock

--- src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
+++ src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
@@ -1,25 +1,7 @@
 package com.epam.prejap.teatrees.block;
 
-import java.util.List;
-import java.util.Random;
-import java.util.function.Supplier;
 
-public class BlockFeed {
-
-    private final Random rnd = new Random();
-    private final List<Supplier<Block>> blocks = List.of(
-        OBlock::new, IBlock::new
-    );
-
-    public BlockFeed() {
-    }
-
-    public Block nextBlock() {
-        return blocks.get(rnd.nextInt(blocks.size())).get();
-    }
-
-    public Block nextBlock(int i) {
-        return blocks.get(i).get();
-    }
+public interface BlockFeed {
+    Block nextBlock();
 
 }
---
+++ src/main/java/com/epam/prejap/teatrees/block/RandomBlockFeed.java
@@ -0,0 +1,22 @@
+package com.epam.prejap.teatrees.block;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class RandomBlockFeed implements BlockFeed {
+
+    private final Random rnd = new Random();
+    private final List<Supplier<Block>> blocks = List.of(
+        OBlock::new
+    );
+
+    public RandomBlockFeed() {
+    }
+
+    @Override
+    public Block nextBlock() {
+        return blocks.get(rnd.nextInt(blocks.size())).get();
+    }
+
+}
