53de33602a4bf2d35deca42da586bed8a892ac68
Pawel Kierat <pawel_kierat@epam.com>
2022-03-24 11:16:27 +0100

    Cherry picked branch 6

--- src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
+++ src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
@@ -1,21 +1,5 @@
 package com.epam.prejap.teatrees.block;
 
-import java.util.List;
-import java.util.Random;
-import java.util.function.Supplier;
-
-public class BlockFeed {
-
-    private final Random rnd;
-    private final List<Supplier<Block>> blocks = List.of(
-         OBlock::new, UBlock::new
-    );
-
-    public BlockFeed(Random random) {
-        this.rnd = random;
-    }
-
-    public Block nextBlock() {
-        return blocks.get(rnd.nextInt(blocks.size())).get();
-    }
+public interface BlockFeed {
+    Block nextBlock();
 }
