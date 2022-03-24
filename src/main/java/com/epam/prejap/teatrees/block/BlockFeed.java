bd85e43f283ae0e6f5a38a544200f6764757e8be
KrzysztofDolanski <60822481+KrzysztofDolanski@users.noreply.github.com>
2022-03-24 08:15:05 +0100

    Revert "Block named and looks like I was added. TestNG upgraded."

--- src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
+++ src/main/java/com/epam/prejap/teatrees/block/BlockFeed.java
@@ -8,7 +8,7 @@ public class BlockFeed {
 
     private final Random rnd = new Random();
     private final List<Supplier<Block>> blocks = List.of(
-        OBlock::new, IBlock::new
+        OBlock::new
     );
 
     public BlockFeed() {
@@ -16,10 +16,6 @@ public class BlockFeed {
 
     public Block nextBlock() {
         return blocks.get(rnd.nextInt(blocks.size())).get();
-    }
-
-    public Block nextBlock(int i) {
-        return blocks.get(i).get();
     }
 
 }
