d553cdc06d30cdf79a0b608661b9f9a3d1f2c26d
Krzysztof Dolanski <krzysztof_dolanski@epam.com>
2022-03-28 14:55:08 +0200

    merged changes

--- src/test/java/com/epam/prejap/teatrees/block/BlockIT.java
+++ src/test/java/com/epam/prejap/teatrees/block/BlockIT.java
@@ -41,7 +41,8 @@ public class BlockIT {
         return new Object[] {
                 new BlockIT(new OBlock(), "|##|\n|##|\n"),
                 new BlockIT(new TBlock(), "|###|\n| # |\n"),
-                new BlockIT(new LBlock(), "|# |\n|# |\n|##|\n")
+                new BlockIT(new LBlock(), "|# |\n|# |\n|##|\n"),
+                new BlockIT(new IBlock(), "| #|\n| #|\n| #|\n| #|\n")
         };
     }
 
