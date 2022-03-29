313777eaa9fd5d3376cfc6110f683d6b3a0c2f9f
Dominik Kaminski <dominik_kaminski@epam.com>
2022-03-29 20:11:02 +0200

    Add integration tests for Grid and Playfield

--- src/test/java/com/epam/prejap/teatrees/game/PlayfieldVsGridIT.java
+++ src/test/java/com/epam/prejap/teatrees/game/PlayfieldVsGridIT.java
@@ -1,2 +1,81 @@
-package com.epam.prejap.teatrees.game;public class PlayfieldVsGridIT {
+package com.epam.prejap.teatrees.game;
+
+import com.epam.prejap.teatrees.block.Block;
+import com.epam.prejap.teatrees.block.BlockFeed;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class PlayfieldVsGridIT {
+    private final BlockFeed feed = () -> new Block(new byte[][] {{1,1},{1,1}}) {};
+    private OutputStream out;
+    private Playfield playfield;
+
+    @BeforeMethod
+    void setUp() {
+        out = new ByteArrayOutputStream();
+        playfield = new Playfield(new Grid(4,4), feed, new Printer(new PrintStream(out)));
+    }
+
+    @Test
+    public void nextBlockShouldAppearOnGrid() {
+        // given
+        String expected = """
+                +----+
+                | ## |
+                | ## |
+                |    |
+                |    |
+                +----+
+                """;
+        // when
+        playfield.nextBlock();
+        // then
+        Assert.assertTrue(out.toString().contains(expected));
+    }
+
+    @Test(dataProvider = "blockMovementShouldBeRepresentedOnGridDP")
+    public void blockMovementShouldBeRepresentedOnGrid(Move move, String expected) {
+        // given
+        playfield.nextBlock();
+        // when
+        playfield.move(move);
+        // then
+        Assert.assertTrue(out.toString().endsWith(expected));
+    }
+
+    @DataProvider
+    Object[][] blockMovementShouldBeRepresentedOnGridDP() {
+        return new Object[][] {
+            {Move.LEFT, """
+                +----+
+                |    |
+                |##  |
+                |##  |
+                |    |
+                +----+
+                """},
+            {Move.RIGHT, """
+                +----+
+                |    |
+                |  ##|
+                |  ##|
+                |    |
+                +----+
+                """},
+            {Move.NONE, """
+                +----+
+                |    |
+                | ## |
+                | ## |
+                |    |
+                +----+
+                """},
+        };
+    }
 }
